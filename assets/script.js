21744a5bdb3d5911ba6ed19d383b313db8a09301
Alexi.Arias <161531356+AlexiArias@users.noreply.github.com>
2024-05-16 08:22:58 -0600

    JavaScript code added

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+
+
+/*
+if (topic1 === 'HTML') {
+   console.log("Lest's study HTML!");
+} else if (topic1 === 'CSS'){
+   console.log ("Let's study CSS!");
+} else if (topic1 === 'Git') {
+   console.log ("Let's study Git!");
+} else if (topic1 === 'JavaScript') {
+   console.log("Let's study JavaScript");
+} else {
+   console.log("Please try again");
+}
+ */
+
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (let x = 0; x < topics.length; x++ ) {
+   console.log(topics[x]);
+}
+
+
+
