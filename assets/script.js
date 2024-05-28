dcf548cd14842a696ed1d041c7b3df6f8e8e7e74
Alexi.Arias <161531356+AlexiArias@users.noreply.github.com>
2024-05-28 12:41:51 -0600

    Added functionality to select a random topic

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,29 @@
 
 
-/*
-if (topic1 === 'HTML') {
-   console.log("Lest's study HTML!");
-} else if (topic1 === 'CSS'){
-   console.log ("Let's study CSS!");
-} else if (topic1 === 'Git') {
-   console.log ("Let's study Git!");
-} else if (topic1 === 'JavaScript') {
-   console.log("Let's study JavaScript");
-} else {
-   console.log("Please try again");
-}
- */
-
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let x = 0; x < topics.length; x++ ) {
-   console.log(topics[x]);
+function listTopics() {
+   for (let x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+   }
 }
 
+function selectTopic() {
+   if (randomTopic === 'HTML') {
+   console.log("Lest's study HTML!");
+   } else if (randomTopic === 'CSS') {
+   console.log ("Let's study CSS!");
+   } else if (randomTopic === 'Git') {
+   console.log ("Let's study Git!");
+   } else if (randomTopic === 'JavaScript') {
+   console.log("Let's study JavaScript");
+   } else {
+   console.log("Please try again");
+   }
+}
+console.log('Here are the topics we learned through Prework:')
+listTopics();
 
-
+console.log('which topic should we study first?')
+selectTopic();
